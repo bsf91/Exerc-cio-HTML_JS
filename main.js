7acf8dd4af82ec8d423fe436da390645eee283f6
bsf91 <brunoscartezinifukuda@gmail.com>
2024-05-29 11:48:01 -0300

    Atualizando mensagem de erro

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.getElementById('formA').addEventListener('submit', function(e) {
     if (campoB <= campoA) {
 
     e.preventDefault();
-    alert('O segundo número deve ser maior do que primeiro.');
+    alert('O número no campo A deve ser maior do que o no campo B.');
     } else {
 
     alert('Formulário enviado com sucesso!');
